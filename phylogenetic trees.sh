d47ec052986c0abc203ce4bc488406efa5a35198
Ollyar <or291@exeter.ac.uk>
2025-03-19 16:39:21 +0000

    Phylogenetic trees.sh

--- phylogenetic trees.sh
+++ phylogenetic trees.sh
@@ -3,4 +3,4 @@
 muscle -align HFMG94VAA_RS03475_sequences.fasta -output HFMG94VAA_RS03475.aln
 
 #create the tree
-iqtree -s Virginia_consensus_strains_onlyLucy_1.fasta -B 1000 -T AUTO -m TEST --prefix iqtree_Virginia_consensus_strains_onlyLucy_1
+iqtree -s HFMG94VAA_RS03475.aln -B 1000 -T AUTO -m TEST --prefix iqtree_HFMG94VAA_RS03475
