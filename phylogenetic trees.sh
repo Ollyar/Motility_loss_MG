58ae03cb037a952455321d8dba457994274b938f
Oliver Ravenhill <or291@exeter.ac.uk>
2025-03-24 10:48:19 +0000

    Delete phylogenetic trees.sh

--- phylogenetic trees.sh
+++
@@ -1,6 +0,0 @@
-#usr/bin/bash
-#creates an alingment
-muscle -align HFMG94VAA_RS03475_sequences.fasta -output HFMG94VAA_RS03475.aln
-
-#create the tree
-iqtree -s HFMG94VAA_RS03475.aln -B 1000 -T AUTO -m TEST --prefix iqtree_HFMG94VAA_RS03475
